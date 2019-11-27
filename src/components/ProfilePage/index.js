b9f9ee42af11165c521fc4351d0aed3af9dd5bc9
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-27 11:43:03 -0400

    create profile page component

---
+++ src/components/ProfilePage/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Row, Col } from 'react-materialize';
+import UserInfoContainer from '../../containers/UserInfoContainer';
+import MainLayout from '../MainLayout';
+
+const ProfilePage = () => (
+  <MainLayout>
+    <Row>
+      <Col s={12} m={3}>
+        <UserInfoContainer />
+      </Col>
+      <Col s={12} m={6}>
+        Tweet List
+     </Col>
+      <Col s={12} m={3}>
+        Trending Topics
+     </Col>
+    </Row>
+  </MainLayout>
+);
+
+export default ProfilePage;
