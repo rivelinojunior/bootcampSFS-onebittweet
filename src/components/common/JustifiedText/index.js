fb9b046d452dfa842bd24abeb101c81dc7696caa
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-11-27 11:43:50 -0400

    create Justified text and Row without border components

---
+++ src/components/common/JustifiedText/index.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+const JustifiedText = styled.p`
+ text-align: justify`;
+ export default JustifiedText;
---
+++ src/components/common/RowNoBottomMargin/index.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+import { Row } from 'react-materialize';
+const RowNoBottomMargin = styled(Row)`
+ margin-bottom: 0px`;
+ export default RowNoBottomMargin;
