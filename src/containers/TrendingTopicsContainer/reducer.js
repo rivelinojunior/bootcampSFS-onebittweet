f1b2f5880e1c3564b2c0457730124b6319b702da
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-12-05 12:46:54 -0400

    add constants

--- src/containers/TrendingTopicsContainer/reducer.js
+++ src/containers/TrendingTopicsContainer/reducer.js
@@ -1,10 +1,14 @@
-const initialState = { hashtags: [] };
+import { UPDATE_TRENDINGS } from './constants';
  
-export default function(state = initialState, action) {
-  switch (action.type) {
-   case 'UPDATE_TRENDINGS':
-    return { hashtags: [['#OneBitCode', '100k'], ['#RubyOnRails', '10k']] }
-   default:
+ // The initial state of the App
+ const initialState = { hashtags: [] };
+ 
+ export default function(state = initialState, action) {
+   switch (action.type) {
+    case UPDATE_TRENDINGS:
+     return { ...state, hashtags: action.payload.hashtags }
+    default:
      return state;
-  }
-}
+   }
+ }
+ 
