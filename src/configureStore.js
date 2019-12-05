0939b91bc191908ec032063b5415c93a674f4ddf
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-12-05 17:52:52 -0400

    add routes

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import Reducers from './reducers';
 import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory'
+import { routerMiddleware } from 'react-router-redux'
 
-const Store = createStore(Reducers, applyMiddleware(thunk));
+export const history = createHistory()
+const history_middleware = routerMiddleware(history)
+
+const Store = createStore(Reducers, applyMiddleware(history_middleware, thunk));
 
 export default Store;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import Store from './configureStore';
+import Store, { history } from './configureStore';
+import { ConnectedRouter } from 'react-router-redux';
 
 ReactDOM.render(
-  <Provider store={Store}><App /></Provider>, document.getElementById('root')
+  <Provider store={Store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
