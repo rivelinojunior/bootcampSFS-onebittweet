df866674d999280173feebddf001a7a5c0a26289
Rivelino Junior <rv.fernandes.junior@gmail.com>
2019-12-05 12:35:59 -0400

    create reducer to trendingTopics

---
+++ src/containers/TrendingTopicsContainer/reducer.js
@@ -0,0 +1,10 @@
+const initialState = { hashtags: [] };
+ 
+export default function(state = initialState, action) {
+  switch (action.type) {
+   case 'UPDATE_TRENDINGS':
+    return { hashtags: [['#OneBitCode', '100k'], ['#RubyOnRails', '10k']] }
+   default:
+     return state;
+  }
+}
